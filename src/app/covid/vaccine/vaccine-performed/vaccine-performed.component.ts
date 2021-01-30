aabf1bb970e351251fcb254b50cdc6fa60b0c3a8
frazzaglia <zicarofrancesco@gmail.com>
2021-01-30 21:28:25 +0100

    #50 - Aggiornata sezione vaccini con i dati nazionali (rimossi dalla sezione Italia).

--- src/app/covid/vaccine/vaccine-performed/vaccine-performed.component.ts
+++ src/app/covid/vaccine/vaccine-performed/vaccine-performed.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {take} from 'rxjs/operators';
+import {VaccineService} from '../vaccine.service';
 
 @Component({
   selector: 'app-vaccine-performed',
@@ -7,17 +9,24 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class VaccinePerformedComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private vaccineService: VaccineService
+  ) {}
 
   @Input() ranking: any = {};
 
   bestFive = [];
   worstFive = [];
   fullRanking = [];
+  ageGroups = [];
 
   ngOnInit(): void {
-      this.bestFive = this.ranking.bestRanked;
-      this.worstFive = this.ranking.worstRanked;
-      this.fullRanking = this.ranking.fullRanking;
+    this.bestFive = this.ranking.bestRanked;
+    this.worstFive = this.ranking.worstRanked;
+    this.fullRanking = this.ranking.fullRanking;
+
+    this.vaccineService.getVaccineData().pipe(take(1)).subscribe(data => {
+      this.ageGroups = data.ageGroups;
+    });
   }
 }
